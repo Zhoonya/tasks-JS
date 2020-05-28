f2412bd7f99132fbf05feda9738c445f062437b0
Евгения <zhoonya1@rambler.ru>
2020-05-28 14:20:09 +0300

    Написать функцию, которая принимает на вход одномерный массив и возвращает объект который содержит информацию о количестве вхождений каждого значения.

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ const getRandomNumber = (minValue, maxValue) => {
     return minValue + Math.round(Math.random() * (maxValue - minValue));
 };
 
+const getUniqueValues = (array) => {
+    return Array.from(new Set(array));
+};
+
 const createRandomArr = (lengthOfArray) => {
     return Array.from(Array(lengthOfArray)).map(() => {
         return getRandomNumber(-10, 10)
     });
+};
+
+const getCountOfArrValues = (array) => {
+    const countOfValues ={};
+    for (let i = 0; i < array.length; i++) {
+        if (countOfValues[array[i]] !== undefined) {
+            countOfValues[array[i]]++
+        } else {
+            countOfValues[array[i]] = 1;
+        }
+    }
+    return countOfValues;
 };
